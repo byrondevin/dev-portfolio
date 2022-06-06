bf83bf559813058746dd9421e9cc754377ab4ed9
Byron <99398623+byrondevin@users.noreply.github.com>
2022-06-06 21:47:37 +0700

    before vercel delopyment 2

--- pages/index.js
+++ pages/index.js
@@ -118,6 +118,7 @@ export default function Home() {
             display: block;
           }
           #profileImg{
+            width:250px
             display: block;
             margin-left: auto;
             margin-right: auto;
